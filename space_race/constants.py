646d47dc4cd09363c629326382f0222a49314ef0
Miguel López <lop18028@byui.edu>
2022-04-01 09:26:10 -0600

    Add constants.py file.
    Added the constants file that store global variables for all for the game.

---
+++ space_race/constants.py
@@ -0,0 +1,41 @@
+import arcade
+
+# These are Global constants to use throughout the game.
+
+# General.
+GAME_NAME = "Space Race!"
+BACKGROUND_COLOR = arcade.color.BLACK
+DEFAULT_RADIUS = 5
+DEFAULT_ANGLE = 0
+
+# Screen
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 600
+
+# Game over
+GAME_OVER_TEXT = "GAME OVER!"
+GAME_OVER_FONT_SIZE = 70
+
+# Score.
+SCORE_POSITION = 25
+SCORE_FONT_SIZE = 20
+SCORE_COLOR = arcade.color.WHITE
+
+# Time.
+RACE_TIME = 60 * 1
+TIMER_WIDTH = 8
+TIMER_HEIGHT = SCREEN_HEIGHT
+TIMER_COLOR = arcade.color.WHITE
+
+# Players.
+START_POSITION = 35
+PLAYER_ONE_POSITION = 225
+PLAYER_TWO_POSITION = 675
+PLAYERS_SPEED = 55
+
+# Meteorit.
+METEORIT_RADIUS = 20
+METEORIT_IMAGE = r"space_race\resources\images/meteorit.png"
+
+# Spacecraft.
+SPACECRAFT_IMAGE = r"space_race\resources\images/spacedraft.png"
